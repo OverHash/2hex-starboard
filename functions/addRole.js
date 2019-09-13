eebdc1bd959341ab24ad6333ca46169fa729c3fd
OverHash <46231745+OverHash@users.noreply.github.com>
2019-09-14 11:15:02 +1200

    Update addRole.js
    Allowed for 4 different variants of input and made all the roles uppercase

--- functions/addRole.js
+++ functions/addRole.js
@@ -1,29 +1,27 @@
 const discord = require('discord.js');
 
 const allowedRoles = [
-	'animator',
-	'builder',
-	'composer',
-	'designer',
-	'gamer',
-	'investor',
-	'producer',
-	'programmer',
-	'streamer',
-	'tester',
-	'trader',
-	'translator',
-	'voiceactor',
-	'gamenight',
-	'gamejam',
-	'hiring',
-	'nothiring',
-	'neutral',
-	'buildhelper',
-	'programhelper',
-	'designhelper',
-	'uihelper',
-	'botupdates',
+	'Animator',
+	'Builder',
+	'Composer',
+	'Designer',
+	'Gamer',
+	'Investor',
+	'Producer',
+	'Programmer',
+	'Streamer',
+	'Tester',
+	'Trader',
+	'Translator',
+	'Voice Actor',
+	'Game Night',
+	'Gamejam',
+	'For Hire',
+	'Not For Hire',
+	'Builder Helper',
+	'Program Helper',
+	'Design Helper',
+	'UI Helper',
 ];
 
 module.exports = function(message) {
@@ -34,14 +32,19 @@ module.exports = function(message) {
 
 	const messageContent = message.content.slice(1);
 
-	if (!allowedRoles.find(object => object == messageContent)) {
+	/*
+	if (!allowedRoles.find(object => object === messageContent)) {
 		return;
-	}
+	} */
 
-	const role = guildRoles.find(object => object.name == messageContent);
+	/* first = regular
+	second = lower case
+	third = no space regular
+	fourth = no space lower case */
+	const role = guildRoles.find(object => (object.name === messageContent) || (object.name.toLowerCase() === messageContent.toLowerCase()) || (object.name.replace(/\s/g, '') === messageContent) || (object.name.toLowerCase().replace(/\s/g, '') === messageContent.toLowerCase()));
 
 	/* Check that the role exists */
-	if (!role) {
+	if (!role || !allowedRoles.find(object => object === role.name)) {
 		return;
 	}
 
@@ -54,15 +57,15 @@ module.exports = function(message) {
 		.setTimestamp()
 		.setTitle('Role changes');
 
-	if (memberRoles.find(r => r.name == role.name)) {
+	if (memberRoles.find(r => r.name === role.name)) {
 		/* remove the role */
 		member.removeRole(role, 'User requested to remove this role');
-		responseEmbed.setDescription('✅ Successfully removed the role ' + role.name)
+		responseEmbed.setDescription('✅ Successfully removed the role ' + role.name);
 		responseEmbed.setColor('#FF4136');
 	} else {
 		/* add them the role */
 		member.addRole(role, 'User requested to receive this role');
-		responseEmbed.setDescription('✅ Successfully gave you the role ' + role.name)
+		responseEmbed.setDescription('✅ Successfully gave you the role ' + role.name);
 		responseEmbed.setColor('#32CD32');
 	}
 
