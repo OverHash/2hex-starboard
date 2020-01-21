516da556eb9b8cfa5167f2b6c54a6149708c25e6
OverHash <46231745+OverHash@users.noreply.github.com>
2020-01-21 14:59:24 +1300

    Begin of rebase

---
+++ dist/config.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.settings = {
+    // token/prefix
+    token: "NjEzNTg4NzI3OTk4MDU0NDAy.XejPGw.ZvyxpnCaxHjPscxN3eOllS0AthY",
+    prefix: "-",
+    roleGivePrefix: '!',
+    // starboard stuff
+    communityGuildId: '613587353084231730',
+    starboardGuildId: '613587300655562762',
+    communitySubmissionChannelId: '613587914911383559',
+    reaction: '⭐',
+    reactionsNeeded: 6,
+    // ready config
+    createReadyMessage: true,
+    readyMessageChannelId: '632026661251973150',
+    // colors
+    colors: {
+        "DEFAULT": "3483eb",
+        "SUCCESS": '09deac',
+        "FAILED": 'fc5656',
+        "HAZARD": 'fc9e3a'
+    }
+};
+//# sourceMappingURL=config.js.map
