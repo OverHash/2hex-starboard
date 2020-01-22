b8f9e42e9837f34f0c4f1814a5c24d01fc9e4f63
OverHash <46231745+OverHash@users.noreply.github.com>
2020-01-23 11:38:47 +1300

    command handler

---
+++ src/Util/commands.ts
@@ -0,0 +1,37 @@
+import { Collection, Client, Message } from "discord.js";
+import fs = require('fs');
+import path = require("path");
+
+export type commandProps = {
+	props: {
+		data: {
+			aliases: Array<string>,
+			type: 'mod' | 'everyone',
+			name: string,
+			description: string,
+			usage: Array<string>
+		}
+	},
+
+	run: (client: Client, message: Message, args: Array<string>, errorFunction: (string) => void) => void;
+}
+
+export const commands: Collection<string, commandProps> = new Collection()
+
+fs.readdir(path.join(__dirname, '../commands/'), (err, files) => {
+	if (err) return console.log(err);
+
+	files.forEach(file => {
+		if (!file.endsWith('.js')) return;
+
+		const command: commandProps = require(path.join(__dirname, '../commands/') + file);
+		const name = file.split('.')[0];
+
+		commands.set(name, command)
+
+		// check for aliases for this command
+		for (const alias of command.props.data.aliases) {
+			commands.set(alias, command)
+		}
+	})
+})
