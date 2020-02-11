013f49cb65c38d0062471f7e903041d47ab9d757
OverHash <46231745+OverHash@users.noreply.github.com>
2020-02-11 21:53:52 +1300

    give role on join

---
+++ src/events/guildMemberAdd.ts
@@ -0,0 +1,11 @@
+import { Client, GuildMember } from "discord.js";
+
+module.exports = (client: Client, member: GuildMember) => {
+	const professions = member.guild.roles.get('674373990789152829');
+	const achievements = member.guild.roles.get('674378904647237694');
+
+	if (!professions) throw 'Couldn\'t get role by the name of professions';
+	if (!achievements) throw 'Couldn\'t get role by the name of achievements';
+
+	member.roles.add([professions, achievements], 'Default roles for joining server')
+}
