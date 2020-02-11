fe18fd680192292f298b75a9d540cf6128d88ba0
OverHash <46231745+OverHash@users.noreply.github.com>
2020-02-11 21:54:21 +1300

    base economy

---
+++ src/Util/getEconomy.ts
@@ -0,0 +1,43 @@
+import { GuildMember } from "discord.js";
+import database = require('../Util/database');
+import * as t from "io-ts";
+
+export interface economyData {
+	economyId: number,
+	authorId: string,
+	balance: number,
+	lastDailyRewardClaimTime: string,
+	lastMessageTime: string
+}
+
+const UserStats = t.type({
+	economyId: t.number,
+	authorId: t.string,
+	balance: t.number,
+	lastDailyRewardClaimTime: t.string,
+	lastMessageTime: t.string
+})
+
+export function getEconomyData(member: GuildMember): Promise<economyData> {
+	return new Promise((resolve, reject) => {
+		database.get(`SELECT * FROM economy WHERE authorId='${member.id}'`)
+			.then(userStats => {
+
+				// check if it exists && is valid type
+				if (userStats) {
+					if (!UserStats.is(userStats)) {
+						console.log(userStats);
+						return reject('Expected the match interface economyData, but it didn\'t.');
+					}
+
+					return resolve(userStats);
+				}
+
+				// user has no data, let's create some
+				database.run(`INSERT INTO economy (authorId, balance, lastDailyRewardClaimTime, lastMessageTime) VALUES ('${member.id}', 0, '0', '0');`)
+					.then(() => database.get(`SELECT * FROM economy WHERE authorId = '${member.id}';`))
+					.then((data: economyData) => resolve(data))
+					.catch(err => reject(err));
+			})
+	})
+}
--- src/runners/economy.ts
+++ src/runners/economy.ts
@@ -1,10 +1,32 @@
 import { Client, Message } from "discord.js";
+import database = require("../Util/database");
+import getEconomy = require("../Util/getEconomy");
 
 module.exports = [
 	{
 		name: "message",
 		run: (client: Client, message: Message) => {
-			console.log(message.content + ' was found in economy.ts');
+			if (!message.member) return;
+			if (message.author.bot) return;
+
+			getEconomy.getEconomyData(message.member)
+				.then(stats => {
+					const lastTime = new Date(stats.lastMessageTime);
+
+					const now = new Date();
+
+					if (now.getTime() - lastTime.getTime() >= 1000 * 60) {
+						// user gets some extra dosh
+						stats.lastMessageTime = now.toISOString();
+						// a simple way to calculate a random number between 2 and 5
+						stats.balance += Math.floor(Math.random() * (5 - 2) + 2);
+
+						// update sql database
+						database.run(`UPDATE economy SET lastMessageTime = '${stats.lastMessageTime}', balance = ${stats.balance};`)
+							.catch(err => console.log('Error updating economy for ' + message.author.username + '.\n' + err))
+					}
+				})
+				.catch(err => console.log('Failed to get economy data for ' + message.author.username + ' because ' + err));
 		}
 	}
 ]
