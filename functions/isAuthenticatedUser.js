e794f687dbacffa7173a88f95ded83d8d483f6dc
OverHash <46231745+OverHash@users.noreply.github.com>
2020-06-19 07:36:01 +1200

    move back to discord v11 :smile:

--- functions/isAuthenticatedUser.js
+++ functions/isAuthenticatedUser.js
@@ -5,5 +5,5 @@
  */
 module.exports = function(guildMember) {
 	// community rep, social media rep
-	return guildMember.hasPermission('KICK_MEMBERS') || guildMember.roles.cache.has(501584776180924416) || guildMember.roles.cache.has(501585377228750849)
+	return guildMember.hasPermission('KICK_MEMBERS') || guildMember.roles.has(501584776180924416) || guildMember.roles.has(501585377228750849)
 }
